b52017a9f914406420df3c1121c94f0fa9569d6a
Nick <nickbs114@gmail.com>
2022-10-07 22:32:07 -0400

    Getting project configured on laptop and started on controller

---
+++ Xamine/src/main/java/non/zibatomaso/xamine/controller/UserController.java
@@ -0,0 +1,12 @@
+package non.zibatomaso.xamine.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	
+	
+}
